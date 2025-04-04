2a005e040db4fd14fdbf1e631cb8d86bee624b99
sunminhong <akoda5555@gmail.com>
2025-04-04 21:14:36 +0900

    Step3 Add Navi, List출력

--- SwiftUIMemoNeukkim/SwiftUIMemoNeukkimApp.swift
+++ SwiftUIMemoNeukkim/SwiftUIMemoNeukkimApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoNeukkimApp: App {
+  @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
           MainlistView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
