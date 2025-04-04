f929ee6196910969d56160cb3ffb12fd45427a86
sunminhong <akoda5555@gmail.com>
2025-04-05 02:22:14 +0900

    Step4 View분리, 파일로 다시 분리

---
+++ View/MemoCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemoCell.swift
+//  SwiftUIMemoNeukkim
+//
+//  Created by SunMin Hong on 4/5/25.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoCell()
+}
