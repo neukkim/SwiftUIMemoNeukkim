a9ef94e3c0413ec0585a29f7ed7bc9c6b6a85581
sunminhong <akoda5555@gmail.com>
2025-04-05 16:26:40 +0900

    Step5 메모 쓰기 화면

---
+++ View/ComposeView.swift
@@ -0,0 +1,18 @@
+//
+//  ComposeView.swift
+//  SwiftUIMemoNeukkim
+//
+//  Created by SunMin Hong on 4/5/25.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComposeView()
+}
